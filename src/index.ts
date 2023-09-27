5342c30cb50f8a9959a57bae7fbfced8ddfd8d9b
ByungJoon Lee <imjuni@gmail.com>
2023-09-27 16:53:06 +0900

    Fix interface name: I18nContainerOption > I18nControllerOption

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { default as I18nController } from './I18nController';
 export { default as getI18nContainerOption } from './getI18nContainerOption';
-export type { default as I18nContainerOption } from './interfaces/I18nControllerOption';
+export type { default as I18nControllerOption } from './interfaces/I18nControllerOption';
