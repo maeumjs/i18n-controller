c025249604ec2505c6a7f9b22ea155ccde96feac
ByungJoon Lee <imjuni@gmail.com>
2023-10-09 18:52:29 +0900

    Add new feature: ptu
    - ptu function support `ErrorController` in `@maeum/error-controller`

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { default as I18nController } from './I18nController';
 export { default as getI18nContainerOption } from './getI18nContainerOption';
 export type { default as I18nControllerOption } from './interfaces/I18nControllerOption';
 export { default as pt } from './pt';
+export { default as ptu } from './ptu';
